443626ae091b5b319fb26216c58b207448e325a9
hyz05 <haoyize05@gmail.com>
2025-11-04 18:34:47 +0800

    Add files via upload

--- Lab 5/Appendix B Q1.sql
+++ Lab 5/Appendix B Q1.sql
@@ -1,10 +1,12 @@
-SELECT 
-    m.carpark_id,
-    vd.vehicle_category,
-    COUNT(*) AS vehicle_count
+SELECT
+    pt.carpark_id,
+    COUNT(CASE WHEN v.vehicle_category = 'Car' THEN 1 END) AS num_cars,
+    COUNT(CASE WHEN v.vehicle_category = 'Motorcycle' THEN 1 END) AS num_motorcycles,
+    COUNT(CASE WHEN v.vehicle_category = 'Commercial Vehicle' THEN 1 END) AS num_commercial_vehicles
 FROM Parking_Transaction pt
+JOIN Vehicle_Decom v ON pt.VRN = v.VRN
 JOIN MSCP m ON pt.carpark_id = m.carpark_id
-JOIN Vehicle_Decom vd ON pt.VRN = vd.VRN
-WHERE pt.day_of_week IN ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
-  AND pt.duration_minutes < 60
-GROUP BY m.carpark_id, vd.vehicle_category;
+WHERE pt.duration_minutes < 60
+  AND pt.day_of_week IN ('Monday','Tuesday','Wednesday','Thursday','Friday')
+GROUP BY pt.carpark_id
+ORDER BY pt.carpark_id;
