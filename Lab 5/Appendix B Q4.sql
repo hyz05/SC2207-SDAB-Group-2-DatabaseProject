38dab722969d18715ba995b8e80bd2b9a67b3357
hyz05 <haoyize05@gmail.com>
2025-11-04 18:23:26 +0800

    Add files via upload

--- Lab 5/Appendix B Q4.sql
+++ Lab 5/Appendix B Q4.sql
@@ -1,20 +1,17 @@
 SELECT 
     p.name AS owner_name,
-    v.VRN,
-    COUNT(o.date_time) AS offence_count,
+    v.VRN AS vehicle_registration,
+    COUNT(*) AS offence_count,
     SUM(r.fine) AS total_fines
 FROM Offence o
 JOIN Vehicle_Decom v ON o.VRN = v.VRN
 JOIN Person p ON v.NRIC = p.NRIC
 JOIN Rules r ON o.rule_no = r.rule_no AND o.vehicle_type = r.vehicle_type
-JOIN Accesses a ON o.VRN = v.VRN
-JOIN Resident res ON p.NRIC = res.NRIC
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Accesses a2
-    WHERE a2.carpark_id = o.carpark_id
-      AND a2.unit_number = res.unit_number
-      AND a2.postal_code = res.postal_code
-)
+LEFT JOIN Resident res ON v.NRIC = res.NRIC
+LEFT JOIN Accesses a 
+       ON o.carpark_id = a.carpark_id 
+      AND res.postal_code = a.postal_code
+      AND res.unit_number = a.unit_number
+WHERE a.carpark_id IS NULL 
 GROUP BY p.name, v.VRN
-ORDER BY total_fines DESC;
+ORDER BY owner_name;
